ee129b37d15aed0e5a2b3128c0e406398bd69147
Slipstream <faxcluster20@protonmail.com>
2025-04-14 16:57:36 -0600

    full

--- cogs/games/roll.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext import commands
-import random
-
-async def roll(interaction: discord.Interaction):
-    """Rolls a dice and returns a number between 1 and 6."""
-    result = random.randint(1, 6)
-    await interaction.response.send_message(f"You rolled a **{result}**! 🎲")
